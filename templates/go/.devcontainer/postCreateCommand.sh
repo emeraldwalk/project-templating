f26f47cc71a3f62d381bf5892b43ab8db2eb9153
Brian Ingles <brian@emeraldwalk.com>
2026-06-03 11:53:55 -0500

    go template

---
+++ templates/go/.devcontainer/postCreateCommand.sh
@@ -0,0 +1,27 @@
+#!/bin/bash
+set -e
+
+cd "$(dirname "$0")/.."
+
+
+# Setup agents
+sudo chown -R vscode:vscode /home/vscode/.claude
+
+curl -fsSL https://claude.ai/install.sh | bash
+
+# curl -fsSL https://gh.io/copilot-install | bash
+
+
+# Setup bash
+
+# Setup custom prompt - hybrid of local + container features
+cat >> ~/.bashrc << 'EOF'
+cls ()
+{
+    clear && printf '\033[3J'
+}
+
+# Custom prompt - hybrid of local + container features
+export PS1='\[\]`export XIT=$?; [ "$XIT" -ne 0 ] && echo -n "\[\033[1;31m\]" || echo -n "\[\033[0m\]"`container`export FOLDER=$(basename "$PWD"); export BRANCH="$(git --no-optional-locks symbolic-ref --short HEAD 2>/dev/null || git --no-optional-locks rev-parse --short HEAD 2>/dev/null)"; if [ "${BRANCH:-}" != "" ]; then [ "$FOLDER" != "$BRANCH" ] && echo -n " \[\033[32m\]$FOLDER"; echo -n " \[\033[33m\]($BRANCH)"; else echo -n " \[\033[32m\]$FOLDER"; fi`\[\033[00m\] $ \[\]'
+EOF
+
