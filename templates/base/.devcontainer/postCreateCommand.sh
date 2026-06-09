3c6df3c1ad506eca683149ad886f5b2f0bc1842d
Brian Ingles <brianingles@deephaven.io>
2026-06-09 12:43:31 -0500

    Added iterm coloring

--- templates/base/.devcontainer/postCreateCommand.sh
+++ templates/base/.devcontainer/postCreateCommand.sh
@@ -28,5 +28,12 @@ cls ()
 
 # Custom prompt - hybrid of local + container features
 export PS1='\[\]`export XIT=$?; [ "$XIT" -ne 0 ] && echo -n "\[\033[1;31m\]" || echo -n "\[\033[0m\]"`container`export FOLDER=$(basename "$PWD"); export BRANCH="$(git --no-optional-locks symbolic-ref --short HEAD 2>/dev/null || git --no-optional-locks rev-parse --short HEAD 2>/dev/null)"; if [ "${BRANCH:-}" != "" ]; then [ "$FOLDER" != "$BRANCH" ] && echo -n " \[\033[32m\]$FOLDER"; echo -n " \[\033[33m\]($BRANCH)"; else echo -n " \[\033[32m\]$FOLDER"; fi`\[\033[00m\] $ \[\]'
+
+# Change iTerm2 tab color to green cleanly without empty lines
+if [ -n "$ITERM_SESSION_ID" ] || [ "$TERM_PROGRAM" = "iTerm.app" ] || [ -n "$TMUX" ]; then
+  echo -ne "\033]6;1;bg;red;brightness;46\a"
+  echo -ne "\033]6;1;bg;green;brightness;204\a"
+  echo -ne "\033]6;1;bg;blue;brightness;113\a"
+fi
 EOF
 
